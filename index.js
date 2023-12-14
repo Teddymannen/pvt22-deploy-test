ce6fc4f20e54676f309d1f796e4881ea99a5edec
Teddymannen <teddy_kross@hotmail.com>
2023-12-14 13:55:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = express();
 const port = 3000;
 
 server.get('*', (req, res) => {
-  res.send('<h1>leon was here</h1> <script>alert("Hello World")</script>');
+  res.send('<h1>leon was here</h1> <script>alert("Hello Earth hehe")</script>');
 });
 
 server.listen(port, () => {
